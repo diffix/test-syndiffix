41b9c8204287987487dc5c265fd4cb1ac41a2782
Paul Francis <paul@francis.com>
2023-05-22 08:56:39 +0200

    Update summarize.py

--- test_syndiffix/summarize.py
+++ test_syndiffix/summarize.py
@@ -257,7 +257,8 @@ def setLabelSampleCount(s, labels):
         if label not in sCounts:
             continue
         count = sCounts[label]
-        newLabels.append(f"{label}  ({count})")
+        #newLabels.append(f"{label}  ({count})")
+        newLabels.append(f"{label}")
     return newLabels
 
 def doMlPlot(tu, df, force, hueCol=None):
