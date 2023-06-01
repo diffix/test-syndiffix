272f7cad0f310d2031b34780efa6ee3202cb8e5a
Paul Francis <paul@francis.com>
2023-06-01 13:14:25 +0200

    Update summarize.py

--- test_syndiffix/summarize.py
+++ test_syndiffix/summarize.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import testUtils
 import gatherResults
 import fire
+import numpy as np
 import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
