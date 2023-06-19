280dfd6df743c547e00f56fe768e240ec4649c84
pdobacz <5735525+pdobacz@users.noreply.github.com>
2023-06-19 14:37:21 +0200

    Revert to default `na_values` in `pd.read_csv`

--- test_syndiffix/misc/csvUtils.py
+++ test_syndiffix/misc/csvUtils.py
@@ -2,15 +2,9 @@ import pandas as pd
 
 from pandas._libs.parsers import STR_NA_VALUES as DEFAULT_NA_VALUES
 
-# Removes ones which are expected to be found also in non-numeric columns.
-NA_VALUES = list(set(DEFAULT_NA_VALUES) - set(['', '#N/A', '#N/A N/A',
-                 '#NA', '<NA>', 'N/A', 'NA', 'NULL', 'None', 'n/a', 'null']))
-
 
 def readCsv(csvPath):
     return pd.read_csv(csvPath,
                        low_memory=False,
                        skipinitialspace=True,
-                       index_col=False,
-                       keep_default_na=False,
-                       na_values=NA_VALUES)
+                       index_col=False)
