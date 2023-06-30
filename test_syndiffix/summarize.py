a2caf99898b06cca087b5e68d0f04ebb59c51a08
Paul Francis <paul@francis.com>
2023-06-30 14:01:54 +0200

    Update summarize.py

--- test_syndiffix/summarize.py
+++ test_syndiffix/summarize.py
@@ -279,7 +279,7 @@ def makeScatterWork(dfBase, dfOther, synMethods, ax, score, hueCol, doLog, limit
     dfMerged = pd.merge(dfBase, dfOther, how='inner', on=['csvFile', 'targetColumn', 'mlMethod'])
     print(f"dfMerged shape {dfMerged.shape}")
     if dfMerged.shape[0] > 200000:
-        print(dfMerged.head())
+        print(dfMerged.iloc[:10].to_string())
         quit()
     # Let's count the number of times that X is greater than Y
     countX = len(dfMerged[dfMerged['rowValue_x'] > dfMerged['rowValue_y']])
