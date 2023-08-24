f146e929c84fe35a847dafe03b17e8e4dd19f987
Paul Francis <paul@francis.com>
2023-08-24 16:59:23 +0200

    Update summarize.py

--- test_syndiffix/summarize.py
+++ test_syndiffix/summarize.py
@@ -279,14 +279,14 @@ def getBest(df, from1, from2, rename):
     df2 = df.query(f"synMethod == '{from2}'")
     if df1.shape[0] == 0 or df2.shape[0] == 0:
         return df
-    dfMerged = pd.merge(df1, df2, how='inner', on=['csvFile', 'targetColumn', 'mlMethod', 'numColumns'])
+    dfMerged = pd.merge(df1, df2, how='inner', on=['csvFile', 'targetColumn', 'mlMethod', 'numColumns', 'numRows'])
     dfMerged['rowValue'] = np.where(dfMerged['rowValue_x'] > dfMerged['rowValue_y'],
                                     dfMerged['rowValue_x'], dfMerged['rowValue_y'])
     dfMerged['mlPenalty'] = np.where(dfMerged['rowValue_x'] > dfMerged['rowValue_y'],
                                     dfMerged['mlPenalty_x'], dfMerged['mlPenalty_y'])
     dfMerged['synMethod'] = rename
-    df1 = df[['synMethod', 'rowValue', 'csvFile', 'targetColumn', 'mlMethod', 'mlPenalty', 'numColumns']]
-    df2 = dfMerged[['synMethod', 'rowValue', 'csvFile', 'targetColumn', 'mlMethod', 'mlPenalty', 'numColumns']]
+    df1 = df[['synMethod', 'rowValue', 'csvFile', 'targetColumn', 'mlMethod', 'mlPenalty', 'numColumns', 'numRows']]
+    df2 = dfMerged[['synMethod', 'rowValue', 'csvFile', 'targetColumn', 'mlMethod', 'mlPenalty', 'numColumns', 'numRows']]
     return pd.concat([df1, df2], axis=0)
 
 
