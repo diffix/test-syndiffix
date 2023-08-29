bd6b45c2df05fb1c6b5b8861eca3d22ab3824673
Paul Francis <paul@francis.com>
2023-08-29 08:50:22 +0200

    sdmManager part of synthpop

---
+++ test_syndiffix/oneSynthpopJob.py
@@ -0,0 +1,32 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import testUtils
+import fire
+import pprint
+import sdmTools
+
+pp = pprint.PrettyPrinter(indent=4)
+
+''' This is used in SLURM to run one synthpop job
+'''
+
+def oneSynthPopJob(jobNum=0, expDir='exp_base', numJobs=None, limitToFeatures=False, force=False):
+    tu = testUtils.testUtilities()
+    tu.registerExpDir(expDir)
+    sdmt = sdmTools.sdmTools(tu)
+    if numJobs:
+        realJobNum = jobNum % numJobs
+        sampleNum = int(jobNum / numJobs)
+    else:
+        realJobNum = jobNum
+        sampleNum = 0
+    sdmt.runSynthPopJob(realJobNum, sampleNum, limitToFeatures, force=force)
+
+
+def main():
+    fire.Fire(oneSynthPopJob)
+
+
+if __name__ == '__main__':
+    main()
