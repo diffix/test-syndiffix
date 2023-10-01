7525eba9e7b0f4ec686a4b58a7fb5338d156ebef
Paul Francis <paul@francis.com>
2023-10-01 11:52:37 +0200

    Update loadDataToPostgres.py

--- test_syndiffix/misc/loadDataToPostgres.py
+++ test_syndiffix/misc/loadDataToPostgres.py
@@ -2,18 +2,34 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import pandas as pd
+import psycopg2
 import json
 import pprint
 from misc.csvUtils import readCsv
 
 pp = pprint.PrettyPrinter(indent=4)
 
+pgHost = os.environ['GDA_POSTGRES_HOST']
+pgUser = os.environ['GDA_POSTGRES_USER']
+pgPass = os.environ['GDA_POSTGRES_PASS']
+print(f"Connect to {pgHost} as user {pgUser} and password {pgPass}")
+
+connStr = str(
+            f"host={pgHost} port={5432} database={'postgres'} user={pgUser} password={pgPass}")
+conn = psycopg2.connect(connStr)
+cur = conn.cursor()
+quit()
+
+connStr = str(
+            f"host={pgHost} port={5432} dbname={'banking'} user={'direct_user'} password={'demo'}")
+conn = psycopg2.connect(connStr)
+cur = conn.cursor()
+
 expDir = 'exp_combs'
 synMethod = 'sdx_release'
 
 resultsDir = os.path.join(os.environ['AB_RESULTS_DIR'], expDir, 'results', synMethod)
 files = [f for f in os.listdir(resultsDir) if os.path.isfile(os.path.join(resultsDir, f))]
-pp.pprint(files)
 
 for fileName in files:
     if fileName[-5:] != '.json':
@@ -23,4 +39,5 @@ for fileName in files:
     with open(filePath, 'r') as f:
         data = json.load(f)
     pp.pprint(data['colCombsJob'])
+    dfAnon = pd.DataFrame(data['anonTable'], columns=data['colNames'])
     pass
