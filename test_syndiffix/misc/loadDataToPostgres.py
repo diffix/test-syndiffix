13835b44a7ac78baad3a37b87173d94d34d5db41
Paul Francis <paul@francis.com>
2023-10-01 12:50:22 +0200

    Update loadDataToPostgres.py

--- test_syndiffix/misc/loadDataToPostgres.py
+++ test_syndiffix/misc/loadDataToPostgres.py
@@ -9,24 +9,40 @@ from misc.csvUtils import readCsv
 
 pp = pprint.PrettyPrinter(indent=4)
 
+expDir = 'exp_combs'
+synMethod = 'sdx_release'
+databaseName = 'sdx_demo'
+createDatabase = True
+
 pgHost = os.environ['GDA_POSTGRES_HOST']
 pgUser = os.environ['GDA_POSTGRES_USER']
 pgPass = os.environ['GDA_POSTGRES_PASS']
 print(f"Connect to {pgHost} as user {pgUser} and password {pgPass}")
-
 connStr = str(
             f"host={pgHost} port={5432} dbname={'postgres'} user={pgUser} password={pgPass}")
 conn = psycopg2.connect(connStr)
 cur = conn.cursor()
+sql = f"SELECT FROM pg_database WHERE datname = '{databaseName}'"
+cur.execute(sql)
+ans = cur.fetchall()
+print(ans)
+
 quit()
 
+if createDatabase:
+    # Create database
+    sql = f"CREATE DATABASE {databaseName}"
+    print(sql)
+    cur.execute(sql)
+    conn.close()
+    quit()
+
+quit()
 connStr = str(
             f"host={pgHost} port={5432} dbname={'banking'} user={'direct_user'} password={'demo'}")
 conn = psycopg2.connect(connStr)
 cur = conn.cursor()
 
-expDir = 'exp_combs'
-synMethod = 'sdx_release'
 
 resultsDir = os.path.join(os.environ['AB_RESULTS_DIR'], expDir, 'results', synMethod)
 files = [f for f in os.listdir(resultsDir) if os.path.isfile(os.path.join(resultsDir, f))]
